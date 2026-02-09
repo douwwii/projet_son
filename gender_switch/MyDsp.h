f21c89eab0250f7afa307f29e002a01b2b2546d9
PRIOUX Léo <leo.prioux-richard@insa-lyon.fr>
2026-02-09 15:33:05 +0100

    implémentation du filtre passe haut

--- gender_switch/MyDsp.h
+++ gender_switch/MyDsp.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <Arduino.h>
 #include <AudioStream.h>
+#include "HPF.h"
 
 class MyDsp : public AudioStream {
 public:
   MyDsp();
   virtual void update(void);
+  HPF1 hpf_;
+
 
 private:
   audio_block_t* inputQueueArray[1]; // 1 entrée audio (mono)
